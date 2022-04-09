4c3544cb98976755ebca00780c43f0d8e2fff2ff
natha5 <ns539@canterbury.ac.uk>
2022-04-09 22:13:20 +0100

    <add> Simple index page with form

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<form>
+			<h2>Add new employee<h2>
+			<label for="ename">Employee surname:</label><br>
+			<input type="text" id="ename" name="ename"><br>
+			
+			<label for="enum">Employee number:</label><br>
+			<input type="text" id="enum" name="enum">
+			
+			<label for="job">Job:</label><br>
+			<input type="text" id="job" name="job">
+			
+			<label for="mgr">Manager:</label><br>
+			<input type="text" id="mgr" name="mgr">
+			
+			<label for="hiredate">Hiredate:</label><br>
+			<input type="text" id="hiredate" name="hiredate">
+			
+			<label for="sal">Salary:</label><br>
+			<input type="text" id="sal" name="sal">
+			
+			<label for="comm">Comm:</label><br>
+			<input type="text" id="comm" name="comm">
+			
+			<label for="deptno">Department Number:</label><br>
+			<input type="text" id="deptno" name="deptno">
+			
+			<button type="button">Confirm</button>
+		</form>
+		
+		<form>
+			<h2>Add new department<h2>
+			<label for="dname">Department Name:</label><br>
+			<input type="text" id="dname" name="dname"><br>
+			
+			<label for="dnum">Department number:</label><br>
+			<input type="text" id="dnum" name="dnum">
+			
+			<label for="loc">Location:</label><br>
+			<input type="text" id="loc" name="loc">
+			
+			<button type="button">Confirm</button>
+		</form>
+	
+
+		<?php
+
+
+		?>
+
+	</body>
+</html>
