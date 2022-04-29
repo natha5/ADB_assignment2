29ee740862a7eaab8f948cd75d2dbf0e68823478
DavidFilipiak <davidd.filipiak@gmail.com>
2022-04-29 21:49:52 +0100

    added button click functionality and bootstrap

--- index.php
+++ index.php
@@ -4,9 +4,55 @@
 	<head>
 		<title>Database interface</title>
 		<link   rel="stylesheet" href="style.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+		<script>
+			const queryString = window.location.search;
+			const urlParams = new URLSearchParams(queryString);
+
+			// if want to use the query parameters in javascript
+			const isReset = urlParams.get('reset');
+			console.log(isReset);
+
+			function resetDB(){
+				let baseURL = getBaseURL();
+				baseURL += isReset == 'true' ? "" : "?reset=true";
+				window.location.href = baseURL;
+			}
+
+			function getBaseURL(){
+				return window.location.href.split('?')[0];
+			}
+		</script>
 	</head>
 
 	<body>
+		<div class="row mt-4">
+			<div class="col-2">
+				<button type="button" id="resetButton" onclick="resetDB()" name="reset" value="Reset">Reset</button>
+				<hr>
+				<p class="mb-0"><a href="?select=employee">Employee</a></p>
+				<p class="mb-0"><a href="?select=department">Department</a></p>
+
+			</div>
+			<div class="col-10">
+
+			</div>
+		<div>
+			
+		
+
+		<?php
+
+
+			if(isset($_GET['reset'])){
+				echo "Reset pressed";
+			}
+
+		?>
+
+			<!--
 		<form>
 			<h2>Add new employee<h2>
 			<label for="ename">Employee surname:</label><br>
@@ -49,12 +95,7 @@
 			
 			<button type="button">Confirm</button>
 		</form>
-	
-
-		<?php
-
-
-		?>
+		-->
 
 	</body>
 </html>
